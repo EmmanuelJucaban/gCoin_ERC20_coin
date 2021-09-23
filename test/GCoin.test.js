35b9857dd74d797e6f4b449a86d7f7b28d59bf4b
emmanueljucaban <emmanueljucaban@gmail.com>
2021-09-23 03:41:29 -0500

    f - Added test for ensuring that a user cannot send more than the total supply of the coin to other users

--- test/GCoin.test.js
+++ test/GCoin.test.js
@@ -10,9 +10,32 @@ const expect = chai.expect;
 
 
 contract("Test token creation", async (accounts) => {
-  it('should create the tokens in my account', async () => {
-    const instance = await GCoin.deployed();
-    const totalSupply = await instance.totalSupply();
-    expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+
+  let instance;
+  let totalSupply;
+  beforeEach(async () => {
+    instance = await GCoin.deployed();
+    totalSupply = await instance.totalSupply();
   });
+
+
+  const [deployerAccount, receiverAccount, senderACcount] = accounts;
+  it('should create the tokens in my account', async () => {
+    expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+  });
+
+  it('should not allow to send more tokens than available in total', async () => {
+    const deployerBalance = await instance.balanceOf(deployerAccount);
+    expect(instance.transfer(receiverAccount, new BN(deployerBalance + 1))).to.eventually.be.rejected;
+  });
+
+  it('should allow tokens to be sent between accounts', async () => {
+    const amountToSend = 1;
+    expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    expect(instance.transfer(receiverAccount, amountToSend)).to.eventually.be.fulfilled;
+    expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(amountToSend)));
+    expect(instance.balanceOf(receiverAccount)).to.eventually.be.a.bignumber.equal(new BN(amountToSend));
+  });
+
+
 });
